4d6fe148c9c1a35bcb725ee3f1095b3d3f341455
unknown <nukasine@gmail.com>
2021-05-02 14:14:08 -0600

    added a server.js file, along with a model and routes folder. This will enable me to build the mongoose backend on the app. the routes are commented out until I code the routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+// const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT ||  5050;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+// app.use(express.static(path.join(__dirname, 'public')))s
+
+
+//does not work as of now because these routes currently do not export, so the require returns an error. Uncomment when routes are done.
+// require('./routes/home.js')(app);
+// require('./routes/api.js')(app);
+
+app.listen(PORT, () => {
+  console.log(`Now listening on post ${PORT}`);
+});
