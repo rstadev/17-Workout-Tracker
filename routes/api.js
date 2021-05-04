411af3842033c54aa91dc7139db2715cb1e36ee6
unknown <nukasine@gmail.com>
2021-05-03 19:27:27 -0600

    got a single get route for the api. Seeded database and set mongoose connection in server.js. App now reeds seeded data and displays last workout

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,21 @@
-const path = require('path');
+// const path = require('path');
+const db = require('../models')
 
 module.exports = (app) => {
+  app.get('/api/workouts', (req,res) => {
+    db.Workout.find({}, (err, workouts) => {
+    // .then(data => {res.json(data)})
+    // .catch(err => {
+    //   console.log(err)
+    //   res.json(err)
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(workouts)
+    };
+    })
+  });
+
+  // app.
 
 };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
+const mongoose = require('mongoose')
 
 const app = express();
 
@@ -7,12 +8,12 @@ const PORT = process.env.PORT ||  5050;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
-// app.use(express.static(path.join(__dirname, 'public')))s
+app.use(express.static(path.join(__dirname, 'public')))
 
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 //does not work as of now because these routes currently do not export, so the require returns an error. Uncomment when routes are done.
 require('./routes/home.js')(app);
-// require('./routes/api.js')(app);
+require('./routes/api.js')(app);
 
 app.listen(PORT, () => {
   console.log(`Now listening on post ${PORT}`);
